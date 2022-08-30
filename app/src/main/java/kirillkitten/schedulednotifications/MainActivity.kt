17f797d447355a851bafd141c826af2bfe55d12d
Kirill Kitten <khudiakovk@gmail.com>
2022-08-30 15:54:21 +0300

    display simple notification

--- app/src/main/java/kirillkitten/schedulednotifications/MainActivity.kt
+++ app/src/main/java/kirillkitten/schedulednotifications/MainActivity.kt
@@ -1,5 +1,8 @@
 package kirillkitten.schedulednotifications
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,11 +13,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.getSystemService
 import kirillkitten.schedulednotifications.ui.theme.ScheduledNotificationsTheme
 
+private const val CHANNEL_ID = "kirillkitten.schedulednotifications.DEFAULT_CHANNEL_ID"
+
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             ScheduledNotificationsTheme {
                 // A surface container using the 'background' color from the theme
@@ -26,6 +36,32 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        createNotificationChannel()
+        showNotification()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Default Notification Channel"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+            getSystemService<NotificationManager>()?.run {
+                createNotificationChannel(channel)
+            }
+        }
+    }
+
+    private fun showNotification(){
+        val id = 1
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("1")
+            .setContentText("ru.wikipedia.org")
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .setAutoCancel(true)
+            .build()
+        NotificationManagerCompat.from(this).notify(id, notification)
     }
 }
 
